723ec052cb427e71a11b7495854a2348477f8f78
Seokwon5 <tjrdnjs5@gmail.com>
2022-10-12 12:07:31 +0900

    Initial Commit

---
+++ Lender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lender
+//
+//  Created by 이석원 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
